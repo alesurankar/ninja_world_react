37f461762d52ea65f018de322ecece502452228e
ales2026 <ale.urankar@gmail.com>
2025-12-30 09:59:40 +0100

    adding submit logic

--- src/pages/public/components/AuthForm.tsx
+++ src/pages/public/components/AuthForm.tsx
@@ -2,12 +2,47 @@ import { useState } from "react";
 import { Login } from "../../../assets/images/images";
 import { Signup } from "../../../assets/images/images";
 import Button from "../../../utils/Button";
+
+
 interface AuthFormProps {
   onSuccess: () => void;
 }
 
 const AuthForm = ({ onSuccess }: AuthFormProps) => {
   const [mode, setMode] = useState<"login" | "signup">("login");
+
+  const [form, setForm] = useState({
+    identifier: "",
+    username: "",
+    email: "",
+    password: "",
+  });
+  
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (mode === "login") {
+      console.log("LOGIN DATA:", {
+      identifier: form.identifier,
+      password: form.password,
+    });
+    // call login API
+  } else {
+      console.log("SIGNUP DATA:", {
+      username: form.username,
+      email: form.email,
+      password: form.password,
+    });
+    // call signup API
+  }
+};
 
   return (
     <div className="rounded-xl w-full max-w-4xl bg-white grid grid-cols-1 lg:grid-cols-2">
@@ -27,18 +62,28 @@ const AuthForm = ({ onSuccess }: AuthFormProps) => {
         {/* Content */}
         <div className="text-gray-800 font-semibold text-lg lg:text-xl text-center">
           {mode === "login" ? (
-            <p>
-              <form>
+            <form onSubmit={handleSubmit}>
+              <div>
                 <h2 className="text-black text-xl lg:text-4xl mb-4 lg:mb-8">LOG IN</h2>
                 <input 
-                  type="text" 
-                  placeholder="User" 
-                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
+                  type="text"
+                  name="identifier"
+                  value={form.identifier}
+                  onChange={handleChange}
+                  required
+                  placeholder="User"
+                  className="mb-1 w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"
+                  />
                 <input 
-                  type="password" 
-                  placeholder="Password" 
-                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
-              </form>
+                  type="password"
+                  name="password"
+                  value={form.password}
+                  onChange={handleChange}
+                  required
+                  placeholder="Password"
+                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"
+                  />
+              </div>
               <div className='flex justify-center mt-4 -mb-2'>
                 <Button
                       title='Log In'
@@ -48,53 +93,62 @@ const AuthForm = ({ onSuccess }: AuthFormProps) => {
               </div>
               <div className="mt-6 text-center space-y-2">
                 <span>Don't have an account?</span>
-                <form>
-                  <p className="text-sm lg:text-lg text-gray-800 mt-1">
-                    <input type="checkbox" required/> I agree to the
-                    <a href="#" className="text-sm lg:text-lg text-[#0a9c38] hover:underline">Terms & Conditions</a>
-                  </p> 
-                </form>
                 <button className="text-[#0a9c38] hover:underline mt-4" onClick={() => setMode("signup")}>Signup</button>
               </div>
-            </p>
+            </form>
           ) : (
-            <p>
-              <form>
-                <h2 className="text-black text-xl lg:text-4xl mb-4 lg:mb-8">SIGN UP</h2>
-                <input
-                  type="text"
-                  name="username"
-                  required
-                  autoComplete="username"
-                  placeholder="Username"
-                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
-                <input 
-                  type="email" 
-                  name="email"
-                  required
-                  autoComplete="email" 
-                  placeholder="E-mail" 
-                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
-                <input 
-                  type="password"
-                  name="password"
-                  required 
-                  autoComplete="new-password"
-                  placeholder="Password" 
-                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
+            <div>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <h2 className="text-black text-xl lg:text-4xl mb-4 lg:mb-8">SIGN UP</h2>
+                  <input
+                    type="text"
+                    name="username"
+                    value={form.username}
+                    onChange={handleChange}
+                    required
+                    placeholder="Username"
+                    className="mb-1 w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"
+                    />
+                  <input 
+                    type="email" 
+                    name="email"
+                    value={form.email}
+                    onChange={handleChange}
+                    required
+                    placeholder="E-mail" 
+                    className="mb-1 w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"
+                    />
+                  <input 
+                    type="password"
+                    name="password"
+                    value={form.password}
+                    onChange={handleChange}
+                    required
+                    placeholder="Password" 
+                    className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"
+                    />
+                </div>
+                <div className='flex justify-center mt-4 -mb-2'>
+                  <Button
+                    type="submit"
+                    title='Sign Up'
+                    mainClassName='bg-[#7c2923] hover:bg-[#d5453a]'
+                    titleClassName='text-black'
+                  />
+                </div>
+                <div className="mt-6 text-center space-y-2">
+                  <div>
+                    <p className="text-sm lg:text-lg text-gray-800 mt-1">
+                      <input type="checkbox" required/> I agree to the
+                      <a href="#" className="text-sm lg:text-lg text-[#0a9c38] hover:underline">Terms & Conditions</a>
+                    </p> 
+                  </div>
+                  <span>Already have an account?</span>
+                  <button className="text-[#0a9c38] hover:underline mt-4" onClick={() => setMode("login")}>Login</button>
+                </div>
               </form>
-              <div className='flex justify-center mt-4 -mb-2'>
-                <Button
-                  title='Sign Up'
-                  mainClassName='bg-[#7c2923] hover:bg-[#d5453a]'
-                  titleClassName='text-black'
-                />
-              </div>
-              <div className="mt-6 text-center space-y-2">
-                <span>Already have an account?</span>
-                <button className="text-[#0a9c38] hover:underline mt-4" onClick={() => setMode("login")}>Login</button>
-              </div>
-            </p>
+            </div>
           )}
         </div>
       </div>
--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -1,4 +1,5 @@
 interface Props {
+    type?: "button" | "submit" | "reset";
     title?: string;
     titleClassName?: string;
     color?: string;
@@ -8,10 +9,13 @@ interface Props {
 
 const Button = (props: Props) => {
     return (
-        <div className={`${props.mainClassName} w-40 h-12 lg:w-52 lg:h-16 flex items-center justify-center rounded-xl cursor-pointer`}
-            onClick={props.onClick}>
-            <p className={`${props.titleClassName} text-lg text-textColor`}>{props.title}</p>
-        </div>
+        <button
+            type={props.type}
+            onClick={props.onClick}
+            className={`${props.mainClassName} w-40 h-12 lg:w-52 lg:h-16 flex items-center justify-center rounded-xl cursor-pointer`}
+            >
+            <span className={`${props.titleClassName} text-lg text-textColor`}>{props.title}</span>
+        </button>
     )
 }
 
