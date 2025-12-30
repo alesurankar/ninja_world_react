177f55a8f2539398d68b61014d1b7bc58609257b
ales2026 <ale.urankar@gmail.com>
2025-12-30 07:40:09 +0100

    better style on AuthForm and adding extra input fields

--- src/pages/public/components/AuthForm.tsx
+++ src/pages/public/components/AuthForm.tsx
@@ -10,9 +10,9 @@ const AuthForm = ({ onSuccess }: AuthFormProps) => {
   const [mode, setMode] = useState<"login" | "signup">("login");
 
   return (
-    <div className="rounded-xl w-full max-w-4xl bg-white grid grid-cols-2">
+    <div className="rounded-xl w-full max-w-4xl bg-white grid grid-cols-1 lg:grid-cols-2">
       {/* LEFT PANEL */}
-      <div className="bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${mode === "login" ? Login : Signup})`}}>
+      <div className="bg-cover bg-center p-4 bg-no-repeat" style={{backgroundImage: `url(${mode === "login" ? Login : Signup})`}}>
         {/* Content */}
         <div className="text-white font-bold text-xl">
           {mode === "login" ? (
@@ -23,58 +23,79 @@ const AuthForm = ({ onSuccess }: AuthFormProps) => {
         </div>
       </div>
       {/* RIGHT PANEL */}
-      <div className="bg-[#4a5748] p-10">
+      <div className="bg-[#4a5748] py-6 lg:py-12 px-10 lg:px-12">
         {/* Content */}
-        <div className="text-gray-800 font-semibold text-lg">
-          <div className="mt-4 lg:text-2xl text-center">
-            {mode === "login" ? (
-              <p>
-                <div>
-                  <h2 className="text-black text-xl lg:text-4xl mb-8">LOG IN</h2>
-                  <input 
-                    type="text" 
-                    placeholder="Username" 
-                    className="w-full max-w-sm border rounded px-3 py-2 placeholder:text-sm lg:placeholder:text-lg">  
-                  </input>
-                </div>
-                <div className='flex justify-center mt-4 -mb-2'>
-                  <Button
-                        title='Log In'
-                        mainClassName='bg-[#7c2923] hover:bg-[#d5453a]'
-                        titleClassName='text-black'
-                      />
-                </div>
-                <br />
-                <span className="my-5">Don't have an account?</span>
-                <br />
-                <div>
+        <div className="text-gray-800 font-semibold text-lg lg:text-xl text-center">
+          {mode === "login" ? (
+            <p>
+              <form>
+                <h2 className="text-black text-xl lg:text-4xl mb-4 lg:mb-8">LOG IN</h2>
+                <input 
+                  type="text" 
+                  placeholder="User" 
+                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
+                <input 
+                  type="password" 
+                  placeholder="Password" 
+                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
+              </form>
+              <div className='flex justify-center mt-4 -mb-2'>
+                <Button
+                      title='Log In'
+                      mainClassName='bg-[#7c2923] hover:bg-[#d5453a]'
+                      titleClassName='text-black'
+                    />
+              </div>
+              <div className="mt-6 text-center space-y-2">
+                <span>Don't have an account?</span>
+                <form>
                   <p className="text-sm lg:text-lg text-gray-800 mt-1">
                     <input type="checkbox" required/> I agree to the
                     <a href="#" className="text-sm lg:text-lg text-[#0a9c38] hover:underline">Terms & Conditions</a>
                   </p> 
-                </div>
+                </form>
                 <button className="text-[#0a9c38] hover:underline mt-4" onClick={() => setMode("signup")}>Signup</button>
-              </p>
-            ) : (
-              <p>
-                <div>
-                  <h2 className="text-black text-xl lg:text-4xl mb-8">SIGN UP</h2>
-                  <input type="text" placeholder="Username" className="w-full max-w-sm border rounded px-3 py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
-                </div>
-                <div className='flex justify-center mt-4 -mb-2'>
-                  <Button
-                    title='Sign Up'
-                    mainClassName='bg-[#7c2923] hover:bg-[#d5453a]'
-                    titleClassName='text-black'
-                  />
-                </div>
-                <br/>
-                <span className="">Already have an account?</span>
-                <br/>
+              </div>
+            </p>
+          ) : (
+            <p>
+              <form>
+                <h2 className="text-black text-xl lg:text-4xl mb-4 lg:mb-8">SIGN UP</h2>
+                <input
+                  type="text"
+                  name="username"
+                  required
+                  autoComplete="username"
+                  placeholder="Username"
+                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
+                <input 
+                  type="email" 
+                  name="email"
+                  required
+                  autoComplete="email" 
+                  placeholder="E-mail" 
+                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
+                <input 
+                  type="password"
+                  name="password"
+                  required 
+                  autoComplete="new-password"
+                  placeholder="Password" 
+                  className="w-full max-w-sm border rounded px-2 lg:px-3 py-0 lg:py-2 placeholder:text-sm lg:placeholder:text-lg"></input>
+              </form>
+              <div className='flex justify-center mt-4 -mb-2'>
+                <Button
+                  title='Sign Up'
+                  mainClassName='bg-[#7c2923] hover:bg-[#d5453a]'
+                  titleClassName='text-black'
+                />
+              </div>
+              <div className="mt-6 text-center space-y-2">
+                <span>Already have an account?</span>
                 <button className="text-[#0a9c38] hover:underline mt-4" onClick={() => setMode("login")}>Login</button>
-              </p>
-            )}
-          </div>
+              </div>
+            </p>
+          )}
         </div>
       </div>
     </div>
--- src/utils/Button.tsx
+++ src/utils/Button.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 const Button = (props: Props) => {
     return (
-        <div className={`${props.mainClassName} w-52 h-16 flex items-center justify-center rounded-xl cursor-pointer`}
+        <div className={`${props.mainClassName} w-40 h-12 lg:w-52 lg:h-16 flex items-center justify-center rounded-xl cursor-pointer`}
             onClick={props.onClick}>
             <p className={`${props.titleClassName} text-lg text-textColor`}>{props.title}</p>
         </div>
